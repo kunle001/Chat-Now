e72b1d5fb95616d17caf80c1ad0989f25bea36a4
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-05-20 23:42:47 +0100

    server setup

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { userJoin, userLeave, getRoomUsers } from './utils/users.js';
+import { formatMessage } from './utils/messages.js'
+
+
 
 const app = express();
 const server = http.createServer(app);
@@ -14,10 +18,23 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Coding Kunle'
+
 io.on('connection', (socket) => {
   // socket represents an actual user
-  socket.on('joinRoom', ({ username, room }) => {
-    console.log(username, room);
+  socket.on('joinRoom', ({ username, roomName }) => {
+    const user = userJoin(socket.id, username, roomName);
+    socket.join(user.room);
+
+    // welcome message
+
+    socket.emit('message', formatMessage(botName, "welcome to Chat Now!"));
+
+    socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    })
   })
 });
 
