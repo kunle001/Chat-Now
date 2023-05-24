53a95f91404de0c1da3590fc63f12ad977a406ce
kunle002 <kunle@poem.cards>
2023-05-24 18:49:36 +0100

    added Database

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { userJoin, userLeave, getRoomUsers, getCurrentUser } from './utils/users.js';
-import { formatMessage } from './utils/messages.js'
-
+import { formatMessage } from './utils/messages.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
+import { userRoute } from './routes/userR.js';
+import { messageRoute } from './routes/messageR.js';
+import session from 'express-session'
+import { RoomRoute } from './routes/roomR.js';
+import cookieParser from 'cookie-parser';
+import { currentUser } from '@kunleticket/common';
+import cors from 'cors'
 
 
 const app = express();
@@ -16,7 +25,22 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//config file setting
+dotenv.config({ path: './config.env' });
+
+// utilities for api
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(session({ secret: process.env.SSEC }));
+app.use(cors())
+app.use(cookieParser());
+
+app.use(currentUser)
+
+// routes
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/messages', messageRoute);
+app.use('/api/v1/room', RoomRoute);
 
 const botName = 'Coding Kunle';
 
@@ -30,7 +54,6 @@ io.on('connection', (socket) => {
     socket.join(user.room);
 
     // welcome message
-
     socket.emit('message', formatMessage(botName, "welcome to Chat Now!"));
 
     socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
@@ -61,7 +84,16 @@ io.on('connection', (socket) => {
 });
 
 
+
+const DB = process.env.DB
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('DB connection successful'))
+
 const port = 3000;
+
 
 server.listen(port, () => {
   console.log('app is listening on port 3000');
