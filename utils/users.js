286b78bac503f1dc09d34f520678f74d5244c81c
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-05-24 21:14:03 +0100

    starting consuming api

--- utils/users.js
+++ utils/users.js
@@ -1,7 +1,18 @@
+import axios from "axios";
+
 const users = []
 
-function userJoin(id, username, room) {
-  const user = { id, username, room };
+async function userJoin(id, username, room) {
+  const res = await axios({
+    method: 'POST',
+    url: 'http://127.0.0.1:3000/api/v1/users/sign-up',
+    data: {
+      name: username
+    },
+
+  });
+
+  const user = { id, username: res.data.name, room };
 
   users.push(user)
   return (user)
